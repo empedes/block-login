3c4f19b92c1248420f227e999743834dfaccb287
empedes <100955596+empedes@users.noreply.github.com>
2022-03-08 19:21:26 +0100

    Tłumaczenie EN

--- src/Components/Message.tsx
+++ src/Components/Message.tsx
@@ -8,7 +8,7 @@ interface IMessageProps {
 const Message = ({result, message}: IMessageProps): ReactElement => {
   if (result) {
     const href = `https://explorer.smartkeyplatform.io/testnet/tx/${message}`
-    return <a href={href}>Zobacz w blockchain</a>
+    return <a href={href}>See in blockchain</a>
   } else {
     return <p>{message}</p>
   }
